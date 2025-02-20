564cea114bfa48bc8e64d30cf28b740d639fb035
Kiet Chau <kiet.chau@mohawkcollege.ca>
2025-02-19 21:29:30 -0500

    Add routes for Manage, Search, and About pages

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Manage', function () {
+    return view('welcome');
+});
+
+Route::get('/Search', function () {
+    return view('welcome');
+});
+
+Route::get('/About', function () {
+    return view('welcome');
+});
+
+
