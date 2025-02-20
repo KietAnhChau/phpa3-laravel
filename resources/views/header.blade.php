81701cec3a78d785135449dfc4e4eea6053b1a70
Kiet Chau <kiet.chau@mohawkcollege.ca>
2025-02-19 21:45:41 -0500

    Add header component with logo and user dropdown

---
+++ resources/views/header.blade.php
@@ -0,0 +1,29 @@
+
+<nav class="bg-white border-b border-stone-200 dark:bg-neutral-800 dark:border-neutral-700" id="header">
+    <div class="max-w-[85rem] flex justify-between lg:grid lg:grid-cols-3 basis-full items-center w-full mx-auto py-2.5 px-4 sm:px-6 lg:px-8">
+        <div class="flex items-center">
+            <div class="hidden sm:block">
+                <!-- Logo -->
+                <a class="flex-none rounded-md text-xl inline-block font-semibold focus:outline-none focus:opacity-80" aria-label="KC Logo">
+                    <svg class="w-28 h-auto" width="116" height="32" viewBox="0 0 116 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <text x="10" y="20" font-family="Arial" font-size="20" fill="white">KIET</text>
+                    </svg>
+                </a>
+            </div>
+        </div>
+
+        <div class="hidden lg:block lg:w-full lg:mx-0"></div>
+
+        <div class="flex justify-end items-center gap-x-2">
+            <div class="flex items-center">
+                <div class="hs-dropdown [--placement:top-right] [--strategy:absolute] [--auto-close:inside] relative inline-flex">
+                    <button id="hs-pro-dnad" type="button" class="inline-flex shrink-0 items-center gap-x-1.5 text-start text-stone-800 rounded-full hover:text-stone-600 focus:outline-none focus:text-stone-600 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
+                        <img class="shrink-0 size-[38px] lg:size-8 rounded-full" src="https://images.unsplash.com/photo-1659482633369-9fe69af50bfb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=3&w=320&h=320&q=80" alt="Avatar">
+
+                        <span class="grow hidden lg:block">Kiet Chau</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
