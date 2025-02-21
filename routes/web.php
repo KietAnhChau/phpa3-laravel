24aed894953b9d2ebf7dd2329627cdda5a61c127
Kiet Chau <kiet.chau@mohawkcollege.ca>
2025-02-20 23:50:48 -0500

    Add dummy product data creation and retrieve all products for manage view

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Models\Product;
 
 Route::get('/', function () {
+    // Create some dummy data
+    Product::create([
+        'name' => 'Product 1',
+        'description' => 'Description 1',
+        'price' => 100,
+        'product_code' => 'P001',
+    ]);
+
+    Product::create([
+        'name' => 'Product 2',
+        'description' => 'Description 2',
+        'price' => 200,
+        'product_code' => 'P002',
+    ]);
+
+    Product::create([
+        'name' => 'Product 3',
+        'description' => 'Description 3',
+        'price' => 300,
+        'product_code' => 'P003',
+    ]);
+
+    Product::create([
+        'name' => 'Product 4',
+        'description' => 'Description 4',
+        'price' => 400,
+        'product_code' => 'P004',
+    ]);
+
     return view('home', [
         'name' => 'Home',
     ]);
@@ -14,6 +44,7 @@ Route::get('/manage', function () {
         'name' => 'Manage',
         'isCreate' => false,
         'isEdit' => false,
+        'items' => Product::all(),
     ]);
 })->name('manage');
 
