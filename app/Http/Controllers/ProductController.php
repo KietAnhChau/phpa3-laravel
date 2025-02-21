98675133723feca0d45f32936825439611364b12
Kiet Chau <kiet.chau@mohawkcollege.ca>
2025-02-21 00:11:03 -0500

    Add product management functionality: implement edit, destroy, show, and index methods

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -39,13 +39,14 @@ class ProductController extends Controller
         ]);
     }
 
-    public function edit($id)
+    public function edit(Request $request, Product $product)
     {
         return view('manage', [
             'name' => 'Manage',
             'isCreate' => false,
             'isEdit' => true,
             'items' => Product::all(),
+            'product' => $product,
         ]);
     }
 
@@ -61,6 +62,38 @@ class ProductController extends Controller
         $product->update($validated);
 
         return redirect()->route('manage', [
+            'name' => 'Manage',
+            'isCreate' => false,
+            'isEdit' => false,
+            'items' => Product::all(),
+        ]);
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return redirect()->route('manage', [
+            'name' => 'Manage',
+            'isCreate' => false,
+            'isEdit' => false,
+            'items' => Product::all(),
+        ]);
+    }
+
+    public function show(Product $product)
+    {
+        return view('manage', [
+            'name' => 'Manage',
+            'isCreate' => false,
+            'isEdit' => false,
+            'items' => Product::all(),
+        ]);
+    }
+
+    public function index()
+    {
+        return view('manage', [
             'name' => 'Manage',
             'isCreate' => false,
             'isEdit' => false,
