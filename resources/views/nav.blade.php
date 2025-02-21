418e54e545bca7708748acbddc7dc5caeda6b868
Kiet Chau <kiet.chau@mohawkcollege.ca>
2025-02-20 22:29:41 -0500

    Enhance navigation links with dynamic active state styling

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -4,26 +4,30 @@
     <!-- Nav Links -->
     <div class="w-full flex justify-center items-center">
         <!-- Collapse -->
-        <div id="hs-pro-emh" class="hs-collapse hidden overflow-hidden transition-all duration-300 lg:block">
+        <div class="hs-collapse hidden overflow-hidden transition-all duration-300 lg:block">
             <div class="overflow-hidden overflow-y-auto max-h-[75vh]">
                 <div class="lg:flex lg:items-center lg:gap-x-1 py-2 lg:py-0 space-y-1 lg:space-y-0 justify-center">
-                    <a class="flex gap-x-1.5 py-2 px-3 items-center text-sm text-stone-800 rounded-lg hover:bg-stone-100 focus:outline-none focus:bg-stone-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 bg-stone-100 focus:bg-stone-200 dark:bg-neutral-700 dark:focus:bg-neutral-600"
-                        href="../../pro/ecommerce/index.html">
+                    <a class="flex gap-x-1.5 py-2 px-3 items-center text-sm text-stone-800 rounded-lg hover:bg-stone-100 focus:outline-none focus:bg-stone-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700
+                    {{ $name == 'Home' ? 'bg-stone-100 focus:bg-stone-200 dark:bg-neutral-700 dark:focus:bg-neutral-600' : '' }}"
+                        href="{{ route('home') }}">
                         Home
                     </a>
 
-                    <a class="flex gap-x-1.5 py-2 px-3 items-center text-sm text-stone-800 rounded-lg hover:bg-stone-100 focus:outline-none focus:bg-stone-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                        href="../../pro/ecommerce/referrals.html">
+                    <a class="flex gap-x-1.5 py-2 px-3 items-center text-sm text-stone-800 rounded-lg hover:bg-stone-100 focus:outline-none focus:bg-stone-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700
+                    {{ $name == 'Manage' ? 'bg-stone-100 focus:bg-stone-200 dark:bg-neutral-700 dark:focus:bg-neutral-600' : '' }}"
+                        href="{{ route('manage') }}">
                         Manage
                     </a>
 
-                    <a class="flex gap-x-1.5 py-2 px-3 items-center text-sm text-stone-800 rounded-lg hover:bg-stone-100 focus:outline-none focus:bg-stone-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                        href="../../pro/ecommerce/reviews.html">
+                    <a class="flex gap-x-1.5 py-2 px-3 items-center text-sm text-stone-800 rounded-lg hover:bg-stone-100 focus:outline-none focus:bg-stone-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700
+                    {{ $name == 'Search' ? 'bg-stone-100 focus:bg-stone-200 dark:bg-neutral-700 dark:focus:bg-neutral-600' : '' }}"
+                        href="{{ route('search') }}">
                         Search
                     </a>
 
-                    <a class="flex gap-x-1.5 py-2 px-3 items-center text-sm text-stone-800 rounded-lg hover:bg-stone-100 focus:outline-none focus:bg-stone-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                        href="../../pro/ecommerce/reviews.html">
+                    <a class="flex gap-x-1.5 py-2 px-3 items-center text-sm text-stone-800 rounded-lg hover:bg-stone-100 focus:outline-none focus:bg-stone-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700
+                    {{ $name == 'About' ? 'bg-stone-100 focus:bg-stone-200 dark:bg-neutral-700 dark:focus:bg-neutral-600' : '' }}"
+                        href="{{ route('about') }}">
                         About
                     </a>
                 </div>
